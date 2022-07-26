580122b5544f10b8f733c6dc4b0c77e13aa4b163
liesler <liesler_e_costa@hotmail.com>
2022-07-26 13:08:41 -0300

    Add files via upload

---
+++ src/pages/Home.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Header } from '../components/Header';
+import { Task, TasksList } from '../components/TasksList';
+import { TodoInput } from '../components/TodoInput';
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleAddTask(newTaskTitle: string) {
+    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldTasks => [...oldTasks, newTask])
+  }
+
+  function handleToggleTaskDone(id: number) {
+    //TODO - toggle task done if exists
+    const updateTask = tasks.map(task => ({ ...task }))
+
+    const item = updateTask.find(item => item.id === id);
+
+    if(!item){
+      return
+    }
+
+    item.done = !item.done;
+
+    setTasks(updateTask)
+  }
+
+  function handleRemoveTask(id: number) {
+    //TODO - remove task from state
+    const updateTask = tasks.filter(task => task.id !== id)
+
+    setTasks(updateTask)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header tasksCounter={tasks.length} />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <TasksList
+        tasks={tasks}
+        toggleTaskDone={handleToggleTaskDone}
+        removeTask={handleRemoveTask}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB'
+  }
+})
